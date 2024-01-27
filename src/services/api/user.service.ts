a6d165ead7c432b7c7ff62e31cdb2327fca65864
DimRev <dimarev444@gmail.com>
2024-01-27 10:54:34 +0200

    Add demoData to user service

--- src/services/api/user.service.ts
+++ src/services/api/user.service.ts
@@ -27,7 +27,10 @@ const DB_KEY = 'userDB'
 // ! CHECK BACKEND FOR COMPLETABLE REST METHODS FOR HTTP SERVICE ! //
 
 const demoData: User[] = [
-
+  { _id: 'u101', username: 'testUser', email: 'testUser@gmail.com', imgUrl: 'imgUrl' },
+  { _id: 'u102', username: 'testUser2', email: 'testUser2@gmail.com', imgUrl: 'imgUrl2' },
+  { _id: 'u103', username: 'testUser3', email: 'testUser3@gmail.com', imgUrl: 'imgUrl3' },
+  { _id: 'u104', username: 'testUser4', email: 'testUser4@gmail.com', imgUrl: 'imgUrl4' },
 ]
 
 _initDemoData()
