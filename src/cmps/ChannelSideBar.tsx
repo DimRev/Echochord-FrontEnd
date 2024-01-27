33a28b1fd6fe65805cbad73bff7fb4a6d07e2d03
DimRev <dimarev444@gmail.com>
2024-01-27 22:53:03 +0200

    Add TextChannel route Outlet to ChannelSideBar cmp + implement channel selection logic

--- src/cmps/ChannelSideBar.tsx
+++ src/cmps/ChannelSideBar.tsx
@@ -4,37 +4,56 @@ import {
   SelectedServerContextType,
 } from '../context/SelectedServerContext'
 import ChannelLinksCollapsable from './ui/ChannelLinkCollapsable'
-import { OnSelectTextChannel, OnSelectVoiceChannel } from '../pages/ChatPage'
 import ChevronDownIcon from './svgs/ChevronDownIcon'
+import {
+  SelectedChannelContext,
+  SelectedChannelContextType,
+} from '../context/SelectedChannelContext'
+import { Outlet } from 'react-router-dom'
 
-type PropType = {
-  onSelectTextChannel: OnSelectTextChannel
-  onSelectVoiceChannel: OnSelectVoiceChannel
-}
+type PropType = {}
 
-export default function ChannelSideBar({
-  onSelectTextChannel,
-  onSelectVoiceChannel,
-}: PropType) {
+export default function ChannelSideBar({}: PropType) {
   const { selectedServer } = useContext(
     SelectedServerContext,
   ) as SelectedServerContextType
 
+  const { setSelectedTextChannel, setSelectedVoiceChannel } = useContext(
+    SelectedChannelContext,
+  ) as SelectedChannelContextType
+
+  function onSelectTextChannel(TextChannelId: string): void {
+    const textChannel = selectedServer?.textChannels.find(
+      (textChannel) => textChannel.id === TextChannelId,
+    )
+    if (textChannel) setSelectedTextChannel(textChannel)
+  }
+
+  function onSelectVoiceChannel(VoiceChannelId: string): void {
+    const voiceChannel = selectedServer?.voiceChannels.find(
+      (voiceChannel) => voiceChannel.id === VoiceChannelId,
+    )
+    if (voiceChannel) setSelectedVoiceChannel(voiceChannel)
+  }
+
   return (
-    <section className="channel-sidebar">
-      <header className="channel-sidebar-header">
-        <span>{selectedServer?.name}</span> <ChevronDownIcon />
-      </header>
-      <ChannelLinksCollapsable
-        type={'text'}
-        channels={selectedServer?.textChannels}
-        onSelectChannel={onSelectTextChannel}
-      />
-      <ChannelLinksCollapsable
-        type={'voice'}
-        channels={selectedServer?.voiceChannels}
-        onSelectChannel={onSelectVoiceChannel}
-      />
-    </section>
+    <>
+      <section className="channel-sidebar">
+        <header className="channel-sidebar-header">
+          <span>{selectedServer?.name}</span> <ChevronDownIcon />
+        </header>
+        <ChannelLinksCollapsable
+          type={'text'}
+          channels={selectedServer?.textChannels}
+          onSelectChannel={onSelectTextChannel}
+        />
+        <ChannelLinksCollapsable
+          type={'voice'}
+          channels={selectedServer?.voiceChannels}
+          onSelectChannel={onSelectVoiceChannel}
+        />
+      </section>
+      <Outlet />
+    </>
   )
 }
