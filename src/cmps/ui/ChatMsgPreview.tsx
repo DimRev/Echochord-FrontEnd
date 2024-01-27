3dc7d442cbc56059912b14e01a07cd12be2882ea
DimRev <dimarev444@gmail.com>
2024-01-28 00:06:46 +0200

    Add rough layout of ChatMsgPreview

--- src/cmps/ui/ChatMsgPreview.tsx
+++ src/cmps/ui/ChatMsgPreview.tsx
@@ -1,4 +1,6 @@
 import { ChatMsg } from '../../services/api/server.service'
+import { utilService } from '../../services/helpers/util.service'
+import UserAvatar from './UserAvatar'
 
 type PropType = {
   msg: ChatMsg
@@ -7,8 +9,14 @@ type PropType = {
 export default function ChatMsgPreview({ msg }: PropType) {
   return (
     <article className="chat-msg-card">
-      <h4>{msg.user.username}</h4>
-      <p>{msg.msg}</p>
+      <UserAvatar username={msg.user.username} imgUrl={msg.user.imgUrl} />
+      <header className="chat-header">
+        <span className="chat-user-name">{msg.user.username}</span>
+        <span className="chat-time-diff">
+          {utilService.getTimeDifference(msg.createdAt)}
+        </span>
+      </header>
+      <p className="chat-msg-content">{msg.msg}</p>
     </article>
   )
 }
