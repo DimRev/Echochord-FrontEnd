b43ed793e91bb23437008b2f64d800a2db36d584
DimRev <dimarev444@gmail.com>
2024-01-27 12:27:30 +0200

    Edit Renders of ServerAvatars in ServerSideBar

--- src/cmps/ServerSideBar.tsx
+++ src/cmps/ServerSideBar.tsx
@@ -1,3 +1,14 @@
+import { useContext } from 'react'
+import { ServersContexts } from '../context/ServersContext'
+import ServerAvatar from './ui/ServerAvatar'
+
 export default function ServerSideBar() {
-  return <section className="server-sidebar">Server Sidebar</section>
+  const servers = useContext(ServersContexts)
+  return (
+    <section className="server-sidebar">
+      {servers?.map((server, idx) => (
+        <ServerAvatar key={idx} name={server.name} imgUrl={server.imgUrl} />
+      ))}
+    </section>
+  )
 }
