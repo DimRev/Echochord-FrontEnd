2308d5bf2651a658ab360bb33bf9381804019c2a
DimRev <dimarev444@gmail.com>
2024-01-27 12:28:23 +0200

    Add condition for non-existing img url in ServerAvatar

--- src/cmps/ui/ServerAvatar.tsx
+++ src/cmps/ui/ServerAvatar.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
+
 type PropType = {
   imgUrl?: string
   name: string
 }
 export default function ServerAvatar({ imgUrl, name }: PropType) {
+  const [isLoaded, setIsLoaded] = useState<boolean | null>(null)
   function getAbbreviation(name: string): string {
     const word = name.split(' ')
     const abbreviation = word
@@ -10,9 +13,27 @@ export default function ServerAvatar({ imgUrl, name }: PropType) {
       .join('')
     return abbreviation
   }
+
+  function loadImage(imgUrl: string | null | undefined): Promise<boolean> {
+    return new Promise((resolve) => {
+      if (imgUrl === null || imgUrl === undefined) return resolve(false)
+      const img = new Image()
+
+      img.onload = () => resolve(true)
+      img.onerror = () => resolve(false)
+
+      img.src = imgUrl
+    })
+  }
+
+  ;(async () => {
+    const isLoaded = await loadImage(imgUrl)
+    setIsLoaded(isLoaded)
+  })()
+
   return (
     <div className="server-avatar">
-      {imgUrl ? (
+      {imgUrl && isLoaded ? (
         <img src={imgUrl} alt={name} />
       ) : (
         <span>{getAbbreviation(name)}</span>
