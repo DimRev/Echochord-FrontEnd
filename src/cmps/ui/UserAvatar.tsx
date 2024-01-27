4bc3d2e00f71a2fdc16bab1b0e606efca10c4910
DimRev <dimarev444@gmail.com>
2024-01-28 00:06:15 +0200

    Add UserAvatar cmp

---
+++ src/cmps/ui/UserAvatar.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+type PropType = {
+  imgUrl?: string
+  username: string
+}
+export default function UserAvatar({ imgUrl, username }: PropType) {
+  const [isLoaded, setIsLoaded] = useState<boolean | null>(null)
+  function getAbbreviation(name: string): string {
+    const word = name.split(' ')
+    const abbreviation = word
+      .map((word) => word.charAt(0).toUpperCase())
+      .join('')
+    return abbreviation
+  }
+
+  function loadImage(imgUrl: string | null | undefined): Promise<boolean> {
+    return new Promise((resolve) => {
+      if (imgUrl === null || imgUrl === undefined) return resolve(false)
+      const img = new Image()
+
+      img.onload = () => resolve(true)
+      img.onerror = () => resolve(false)
+
+      img.src = imgUrl
+    })
+  }
+
+  ;(async () => {
+    const isLoaded = await loadImage(imgUrl)
+    setIsLoaded(isLoaded)
+  })()
+
+  return (
+    <div className={`user-avatar`}>
+      <div>
+        {imgUrl && isLoaded ? (
+          <img src={imgUrl} alt={username} />
+        ) : (
+          <span>{getAbbreviation(username)}</span>
+        )}
+      </div>
+    </div>
+  )
+}
