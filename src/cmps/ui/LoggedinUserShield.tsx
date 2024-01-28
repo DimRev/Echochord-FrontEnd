2894b33c2d1b98d0ac0b5230cca4070d0788fa12
DimRev <dimarev444@gmail.com>
2024-01-28 15:24:18 +0200

    Connect loggedinUser to LoggedinUserShield

--- src/cmps/ui/LoggedinUserShield.tsx
+++ src/cmps/ui/LoggedinUserShield.tsx
@@ -1,19 +1,32 @@
+import { useContext } from 'react'
 import GearIcon from '../svgs/GearIcon'
 import MicIcon from '../svgs/MicIcon'
 import SpeakerFullIcon from '../svgs/SpeakerFullIcon'
 import UserAvatar from './UserAvatar'
+import {
+  LoggedinUserContext,
+  LoggedinUserContextType,
+} from '../../context/LoggedinUserContext'
 
 export default function LoggedinUserShield() {
-  const username = 'Dima R'
-  const active = 'active'
+  const { loggedinUser } = useContext(
+    LoggedinUserContext,
+  ) as LoggedinUserContextType
+
+  if (loggedinUser === null) return <></>
   return (
     <div className="loggedin-user-shield">
-      <div className="user-logo">
-        <UserAvatar username={username} imgUrl="" />
-      </div>
-      <div className="user-details">
-        <span>{username}</span>
-        <span>{active}</span>
+      <div className="user-content">
+        <div className={`user-logo ${loggedinUser.status}`}>
+          <UserAvatar
+            username={loggedinUser.username}
+            imgUrl={loggedinUser.imgUrl}
+          />
+        </div>
+        <div className="user-details">
+          <span>{loggedinUser.username}</span>
+          <span>{loggedinUser.status}</span>
+        </div>
       </div>
       <div className="user-btns">
         <div className="mic-icon-wrapper">
