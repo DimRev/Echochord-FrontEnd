a6ef3ee78214001b28f34394264874cc9d0518ea
DimRev <dimarev444@gmail.com>
2024-01-30 18:23:05 +0200

    Change ChatTextInput into a form, add Submit form handle to it

--- src/cmps/ui/ChatTextInput.tsx
+++ src/cmps/ui/ChatTextInput.tsx
@@ -1,18 +1,35 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
 import EmojiIcon from '../svgs/EmojiIcon'
 import GifIcon from '../svgs/GifIcon'
 import GiftIcon from '../svgs/GiftIcon'
 import PlusIcon from '../svgs/PlusIcon'
 import StickerIcon from '../svgs/StickerIcon'
 
-export default function ChatTextInput() {
+type PropType = {
+  onSubmitChatMsg(txt: string): void
+}
+
+export default function ChatTextInput({ onSubmitChatMsg }: PropType) {
+  const [txt, setTxt] = useState('')
+
+  function handleSubmit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault()
+    onSubmitChatMsg(txt)
+    setTxt('')
+  }
+
+  function handleChange(ev: ChangeEvent<HTMLInputElement>) {
+    setTxt(ev.target.value)
+  }
+
   return (
-    <section className="text-input">
+    <form className="text-input" onSubmit={handleSubmit}>
       <div className="container left-container">
         <div className="icon-wrapper plus-icon-wrapper">
           <PlusIcon />
         </div>
       </div>
-      <input type="text" />
+      <input type="text" value={txt} onChange={handleChange} />
       <div className="container right-container">
         <div className="icon-wrapper gift-icon-wrapper">
           <GiftIcon />
@@ -27,6 +44,6 @@ export default function ChatTextInput() {
           <EmojiIcon />
         </div>
       </div>
-    </section>
+    </form>
   )
 }
