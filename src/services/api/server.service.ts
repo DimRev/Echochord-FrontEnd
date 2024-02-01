6aa1b45f1b8f743af2bef8eff32219379a6dfd8b
DimRev <dimarev444@gmail.com>
2024-02-01 19:25:47 +0200

    Add msgs to DemoData

--- src/services/api/server.service.ts
+++ src/services/api/server.service.ts
@@ -40,24 +40,50 @@ const demoData: Server[] = [
       { id: "vr103", name: 'Voice Room 3' },
     ], textChannels: [{
       id: 'cr101', name: 'General', chatMsgs: [
-        { id: 'msg101', createdAt: new Date(1706151131192), msg: 'Hi!', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg102', createdAt: new Date(1706251231192), msg: 'What\'s up dude?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
-        { id: 'msg103', createdAt: new Date(1706351331192), msg: 'It\'s all good here! You?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg104', createdAt: new Date(1706451431192), msg: 'Where\'ve you been?', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg101', createdAt: 1706151131192, msg: 'Hi!', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg102', createdAt: 1706251231192, msg: 'What\'s up dude?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg103', createdAt: 1706351331192, msg: 'It\'s all good here! You?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg104', createdAt: 1706451431192, msg: 'Where\'ve you been?', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg105', createdAt: 1706551531192, msg: 'Just got back from vacation!', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg106', createdAt: 1706651631192, msg: 'How was it?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg107', createdAt: 1706751731192, msg: 'Amazing! I went to Hawaii. ️', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg108', createdAt: 1706851831192, msg: 'Lucky! ', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg109', createdAt: 1706951931192, msg: 'Anyone up for a game night this weekend?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg110', createdAt: 1707052031192, msg: 'I\'m down!', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg111', createdAt: 1707152131192, msg: 'Me too!', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg112', createdAt: 1707252231192, msg: 'What should we play?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg113', createdAt: 1707352331192, msg: 'How about Codenames?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg114', createdAt: 1707452431192, msg: 'I\'m terrible at that game ', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg115', createdAt: 1707552531192, msg: 'Maybe we should watch a movie instead? ', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg116', createdAt: 1707652631192, msg: 'Sure, what do you guys want to watch?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+
       ]
     }, {
       id: 'cr102', name: 'Hangout', chatMsgs: [
-        { id: 'msg101', createdAt: new Date(1706450331192), msg: 'This is message 1', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg102', createdAt: new Date(1706450431192), msg: 'This is message 2', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
-        { id: 'msg103', createdAt: new Date(1706451331192), msg: 'This is message 3', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg104', createdAt: new Date(1706451431192), msg: 'This is message 4', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg101', createdAt: 1706450331192, msg: 'Yo', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg102', createdAt: 1706450431192, msg: 'Yoyo', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg103', createdAt: 1706451331192, msg: 'Yoyoyo?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg104', createdAt: 1706451431192, msg: 'Yoyoyo!', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg105', createdAt: 1706451531192, msg: 'What are you guys up to?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg106', createdAt: 1706451631192, msg: 'Just working on some stuff.', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg107', createdAt: 1706451731192, msg: 'Same here.', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg108', createdAt: 1706451831192, msg: 'Any plans for the weekend?', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg109', createdAt: 1706451931192, msg: 'I was thinking of going to see a movie.', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg110', createdAt: 1706452031192, msg: 'Oh, cool! What do you want to see?', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg111', createdAt: 1706452131192, msg: 'I\'m not sure yet. Any suggestions?', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg112', createdAt: 1706452231192, msg: 'I heard the new action movie is really good.', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg113', createdAt: 1706452331192, msg: 'Hmm, I\'m not really into action movies.', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg114', createdAt: 1706452431192, msg: 'What about a comedy?', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg115', createdAt: 1706452531192, msg: 'Yeah, that could be fun.', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg116', createdAt: 1706452631192, msg: 'I\'ll check the listings and see what\'s playing.', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+
       ]
     }, {
       id: 'cr103', name: 'Memes', chatMsgs: [
-        { id: 'msg101', createdAt: new Date(1706440431192), msg: 'This is message 1', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg102', createdAt: new Date(1706441431192), msg: 'This is message 2', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
-        { id: 'msg103', createdAt: new Date(1706451331192), msg: 'This is message 3', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
-        { id: 'msg104', createdAt: new Date(1706451431192), msg: 'This is message 4', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
+        { id: 'msg101', createdAt: 1706440431192, msg: 'This is message 1', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg102', createdAt: 1706441431192, msg: 'This is message 2', user: { _id: 'u102', imgUrl: 'imgUrl2', username: 'testUser2' } },
+        { id: 'msg103', createdAt: 1706451331192, msg: 'This is message 3', user: { _id: 'u101', imgUrl: 'imgUrl1', username: 'testUser1' } },
+        { id: 'msg104', createdAt: 1706451431192, msg: 'This is message 4', user: { _id: 'u103', imgUrl: 'imgUrl3', username: 'testUser3' } },
       ]
     }],
   },
